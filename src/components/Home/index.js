bf0838e4b70a48f024125ee57a839a4e3b62ef1c
Sriram0612458 <121300942+Sriram0612458@users.noreply.github.com>
2024-12-09 01:44:11 +0530

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,197 @@
+import './index.css';
+
+function Home() {
+    return (
+        <div>
+            <div className="main-container">
+                <div className="image-container">
+                    <img className='logo' src="https://s3-alpha-sig.figma.com/img/8be1/c27b/24873ffb8a3345a8dedea9b0d8f9e7ba?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=L0spKcLnDj3-RGpQBgOl4A15XyrTvF-baao9zqhB1Sbc4h8fV0bqAxcS7zgqS2FsjV7o9OxvALHkXRRbgFT3Xp~6PeUjIn~ISRuX16EbEaLdhXEEpKqHxhzNSpqyeJcazv-C-0Fk4L2YhBx966luC0vI7UTh0tfPGFeD0sMakYF2KogtEA3m~mfkw7wOehPZa3kvbcF7t0T~HrGgOXynC6TglKELsRGlVz95fjwecpdCjr99eGIDVFD61c8UYKTCPShu1wAmLXrhq0ps1APbdyq0Vm16-DHw5sjSFHRHiAsWAAmMCJRJFA5-H9-AXr9MVi1H-zb2wDFoRz3KoK0wsQ__" />
+                </div>
+            </div>
+            <div className="sub-main">
+                <h1 className="main-heading">Essential Vitamins</h1>
+                <div className="box-container">
+                    <div className="box1">
+                        <p className='box1-para'>Online Medical Supplies</p>
+                        <p className="box1-para-2">Get Your Vitamins
+                            & Minerals</p>
+                        <button className="button-1">Explore</button>
+                    </div>
+                    <div className="box2">
+                        <img className="medicineImage" src="https://s3-alpha-sig.figma.com/img/6dd8/7f0a/f73aa0189cad884f47381dd92205b7c0?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=V21VVDmU7JxcRB76OOhoAt9ZJzhck8hR9U3L1UvokjwzK916BF2uOVElrkcxN5YP-si~qFGW9mXybipDrvdR9inQNdWLrDUUFadDcDwhwDJ4adJ0UfkwYRnZT-0b2Y9w29vguT4lAW1HMCi344hAaPojIhdUTUnuzzFnqR8X2VtFZXtBgZJYDWU5w5bRDHt1Nlxenbc59CkLE-RpPwl-8Wt6uz8rwl5~X6nDY-R0fyUzHYIp51B4LQF2W32WHBANFHGkAREYP66qtVWdYPpqIVAoba2zRrWozjVW~~unoMkhN0MSl-kap8oS2~IXt2HMzEYndt7tu2DsSbjQue1p9g__" />
+                    </div>
+                    <div className="box3">
+                        <div className="box3-main">
+
+                            <img className="round-logo" src="https://s3-alpha-sig.figma.com/img/1d76/fc2a/470ec862c44115964979f90b5cb3a94e?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l8br3l5hvS1vnQy3WMDDXmvwBbAoZGBF~14OpoaLciq8S325-GN8~oF56m4ULopQlNJDHShbY78MmUGsjZKiZYOrAbdXVyr3HUSNpFdJtbKkVtYhvAUvHp6ghUpHNKNf2H0EhxgXQAdX5jDhFvmlnt6~wcvCWsUCjPhetY5usDdwSlgaddrZxhaKIuaDy8aQ9FqvG5MWZKQTnkBeWZlih89WfilN3Vkv0Iu6w2x8TmzehqQWS-GJFBXyOBMB1X-NSi2AkO4VTnN6MfPeNS-P8DlWm6JYTg4RHFY2CqAtNMenk5f9jmbl4lN-RE-Zn8XYlohyzBU~XMQB~2rnXotnfw__" />
+                            <div>
+                                <p className="box3-para">Vitamins</p>
+                                <p className="box3-para-2">Increased Vitamins and minerals in your diet</p>
+                            </div>
+                        </div>
+                        <div className="box3-main">
+
+                            <img className="round-logo" src="https://s3-alpha-sig.figma.com/img/c0ae/6abf/9917c0af12dcc49a9c0130d3e68e04b1?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dx9BsMA39Mxa2Odn8qXpjgqLgJyWy~dNaB6DWyuRYIcHKphjSwmcU7SAkxdZ7PI3POB9Uyzc7nMnVoElXMw8CgPzRcSmULQkpHMiGrYbDG~Q8DlnJE-hE3NelQMqZNZdOpuTPBcD2GN2o45uUYVUedoMKhr7BJjjq4Bn6KTkZ8kgzrpS0AEGU7GL1TEkF3n80lm8y4WXbhcWvQ3t7sDsmGyXpFYztMuybNF4rLHwuGcQ0GYWpRQa9~jb1uyEynJGetiEPYQmrQaagS2tvkDhCD0TZ~0YZqnSdYFqi3IGnBJH3YiIqdWxdwfe6MEQgJ~EpmW6GtjA0PRSMQBujKvDvg__" />
+                            <div>
+                                <p className="box3-para">Weight Loss</p>
+                                <p className="box3-para-2"> Weight Loss
+                                    Find scientifically proven solutions</p>
+                            </div>
+                        </div>
+                        <div className="box3-main">
+
+                            <img className="round-logo" src="https://s3-alpha-sig.figma.com/img/ad6c/6ad1/2116d12f8d75befd05e66d41425cb402?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dWc~OffEuoFYOrTG-j6GYpo7NN-Y4O29eyv2w4PY1~ndA0~r8ZJogsmhFEggIbt7DOo2KwWhgGJdYo2RZL04W151-jnCLmEga1NVbZadhi07US-q8oBLAeJ~QkANDAPnzlpL2f4o89~21nLtKDEZYITd4HvG1YF80DhhYtubj05OA4f6hmeprvUIFnJ~MUPSU6ywuc63KZhOGnePDiNYC6wVcIODrgb73yX8-FwFmjTpn690lIb5n2U~-GfjI6I9rTCh950Vdy-UlQ7qO7hG4meji~V9EAGgSZ5HoUvIxN9zicR~Mi2mncb8ECgPKe10P04yn6HfTmecbXP5Ox9lTA__" />
+                            <div>
+                                <p className="box3-para">Functional Foods</p>
+                                <p className="box3-para-2">Functional Foods From protein powers to baby formula</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="round-design-container">
+                    <div className="round-design"></div>
+                    <div className="round-design"></div>
+                </div>
+            </div>
+
+            <div className="sub-main-2">
+                <div className='list-1'>
+                    <div className="list-container">
+                        <img className="listImage" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733664875/a.item-icon-e_kpzina.png" />
+                        <p className="listElements">Clinically Studied</p>
+                        <p className='listDescription'>All products that we offer have undergone lab and safety tests</p>
+                    </div>
+                    <div className="list-container">
+                        <img className="listImage" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733664866/a.item-icon-e_1_quki1k.png" />
+                        <p className="listElements">Vegetarian Friendly</p>
+                        <p className='listDescription'>We have a wide selection of vegetarian products to meet your needs</p>
+                    </div>
+                    <div className="list-container">
+                        <img className="listImage" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733664866/a.item-icon-e_2_nciglo.png" />
+                        <p className="listElements">Made in India</p>
+                        <p className='listDescription'>Shop local and explore health products made right here in India</p>
+                    </div>
+                </div>
+                <div className="list-2"><div className="list-container">
+                    <img className="listImage" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733664866/a.item-icon-e_3_ymtpey.png" />
+                    <p className="listElements">Free shipping</p>
+                    <p className='listDescription'>We deliver to your door with no shipping costs on your orders</p>
+                </div>
+                    <div className="list-container">
+                        <img className="listImage" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733664866/a.item-icon-e_4_hlb3vb.png" />
+                        <p className="listElements">No Risk</p>
+                        <p className='listDescription'>We ensure that all products are safe and within their use-by date</p>
+                    </div>
+                    <div className="list-container">
+                        <img className="listImage" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733664866/a.item-icon-e_5_inyso7.png" />
+                        <p className="listElements">GMO free</p>
+                        <p className='listDescription'>Natural, no modified products and derivatives for those who need it</p>
+                    </div>
+                </div>
+            </div>
+            <div className="sub-main-4">
+                <div className="sub-4-container">
+                    <div className="sub-4-container-1">
+                        <p className="ingredientsPara">INGREDIENTS</p>
+                        <h1 className="ingredientsHeading">Better Ingredients</h1>
+                        <p className="ingredientsDescription">Only the best when you choose products offered on our platform - high-quality ingredients for high quality products! </p>
+                    </div>
+                    <img className="IngredientsImages1" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733674232/div.elbzotech-banner-info-wrap_1_ulkiri.png" />
+                    <img className="IngredientsImages1" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733674242/div.elbzotech-banner-info-wrap_6_iuhwfl.png" />
+                </div>
+                <div className="sub-4-container1">
+                    <img className="IngredientsImages" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733674241/div.elbzotech-banner-info-wrap_5_r1plxl.png" />
+                    <img className="IngredientsImages" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733674232/div.elbzotech-banner-info-wrap_4_qiovpd.png" />
+                    <img className="IngredientsImages" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733674232/div.elbzotech-banner-info-wrap_3_xcbikw.png" />
+                    <img className="designImage" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733674232/div.elbzotech-banner-info-wrap_2_qwni9l.png" />
+                </div>
+            </div>
+            <div className="news-container">
+                <p className='blogPara'>OUR BLOG</p>
+                <h1 className='newsheading'>Latest news</h1>
+            </div>
+            <div className="cards-container">
+                <div className="sub-cards-container">
+                    <div className="card-1">
+                        <button className='newsButton'>20 APR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2022 Is Back</p>
+                    </div>
+                    <div className="card-2">
+                        <button className='newsButton'>20 APR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2023 Is Back</p>
+                    </div>
+                </div>
+                <div className="sub-cards-container">
+                    <div className="card-22">
+                        <button className='newsButton'>20 APR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2022 Is Back</p>
+                    </div>
+                    <div className="card-11">
+                        <button className='newsButton'>20 APR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2023 Is Back</p>
+                    </div>
+
+                </div>
+                <div className="sub-cards-container">
+                    <div className="card-1">
+                        <button className='newsButton'>20 APR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2022 Is Back</p>
+                    </div>
+                    <div className="card-2">
+                        <button className='newsButton'>20 APR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2023 Is Back</p>
+                    </div>
+                </div>
+                <div className="sub-cards-container">
+                    <div className="card-22">
+                        <button className='newsButton'>20 APR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2022 Is Back</p>
+                    </div>
+                    <div className="card-11">
+                        <button className='newsButton'>17 MAR</button>
+                        <p className="newsPara">The Covid-19 Epidemic In 2023 Is Back</p>
+                    </div>
+
+                </div>
+            </div>
+            <div className="footer">
+                <div className="footoer-main">
+                    <div className="footer-card1">
+                        <img className="phoneIcon" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733681904/Component-2.svg_1_ujncuf.png" />
+                        <div>
+                            <p className="phoneNumber">Phone Number</p>
+                            <p className="phoneNumber1">+974 3118 1843</p>
+                        </div>
+                    </div>
+                    <div className="footer-card1">
+                        <img className="phoneIcon" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733681904/Component-2-1.svg_uob9ew.png" />
+                        <div>
+                            <p className="phoneNumber">Email Address</p>
+                            <p className="phoneNumber1">Elbrithcqhr@gmail.com</p>
+                        </div>
+                    </div>
+                    <div className="footer-card1">
+                        <img className="phoneIcon" src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733681904/Component-2-2.svg_ajf5y5.png" />
+                        <div>
+                            <p className="phoneNumber">Office Location</p>
+                            <p className="phoneNumber1"> Ambassador Street, Zone 61</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-information">
+                    <div className="footer-logo">
+                        <img src="https://s3-alpha-sig.figma.com/img/8be1/c27b/24873ffb8a3345a8dedea9b0d8f9e7ba?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=L0spKcLnDj3-RGpQBgOl4A15XyrTvF-baao9zqhB1Sbc4h8fV0bqAxcS7zgqS2FsjV7o9OxvALHkXRRbgFT3Xp~6PeUjIn~ISRuX16EbEaLdhXEEpKqHxhzNSpqyeJcazv-C-0Fk4L2YhBx966luC0vI7UTh0tfPGFeD0sMakYF2KogtEA3m~mfkw7wOehPZa3kvbcF7t0T~HrGgOXynC6TglKELsRGlVz95fjwecpdCjr99eGIDVFD61c8UYKTCPShu1wAmLXrhq0ps1APbdyq0Vm16-DHw5sjSFHRHiAsWAAmMCJRJFA5-H9-AXr9MVi1H-zb2wDFoRz3KoK0wsQ__" />
+                    </div>
+                    <p className='footer-information-para'>Your health, physical and emotional well-being is important to us. We are always by your side and have made it even easier for you to find the necessary vitamins.</p>
+                </div>
+                <p className="locationPara">
+                    <span className="spanElement"><img src="https://res.cloudinary.com/dtvqmmc3d/image/upload/v1733684383/Frame_duzydv.png" /></span>
+                    Elbrit Life Sciences Private Limited. C20, BKC, G Block, Mumbai 400051.
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
